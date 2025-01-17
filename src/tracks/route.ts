120c647c97d8b2701bf311960da5bc3d1ce90296
Padillahirpan <padillahirpan8@gmail.com>
2025-01-17 18:37:42 +0700

    feat: adjust tracks feature

--- src/tracks/route.ts
+++ src/tracks/route.ts
@@ -1,90 +1,15 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 import * as trackService from './service'
-import { PrismaClient } from "@prisma/client";
 import { NewTrackRequestSchema, TrackIdSchema, TrackRequestSchema } from "./schema";
 
 const API_TAG = ['Tracks']
 
-const prisma = new PrismaClient();
-
 export const trackRoute = new OpenAPIHono()
-  // seed tracks
-  .openapi(
-    {
-      method: 'post',
-      path: 'seed',
-      description: 'Create a dummy tracks in the database',
-      responses: {
-        201: {
-            description: 'Tracks successfully added',
-        },
-      },
-      tags: API_TAG,
-    },
-    async (c) => {
-      const tracks = await trackService.seedTracks();
-      return c.json(tracks, 201);
-    },
-  )
-  .openapi(
-    {
-      method: 'get',
-      path: '/',
-      description: 'Get all tracks',
-      responses: {
-        200: {
-          description: 'List of tracks',
-        },
-      },
-      tags: API_TAG, 
-    },
-    async (c) => {
-      const tracks = await trackService.getTracksWithAlbums();
-
-      return c.json(
-        {
-          status: 'success',
-          message: 'Success get all tracks',
-          data: tracks
-        },
-        200
-      );
-    },
-  )
-  .openapi(
-    {
-      method: 'get',
-      path: '/detail/{id}',
-      description: 'Get track by id',
-      request: {
-        params: TrackIdSchema,
-     },
-      responses: {
-        200: {
-          description: 'Details of track',
-        },
-      },
-      tags: API_TAG, 
-    },
-    async (c) => {
-      const trackId = Number(c.req.param('id'));
-      const track = await trackService.getTrackById(trackId);
-
-      return c.json(
-        {
-          status: 'success',
-          message: 'Success get track',
-          data: track
-        },
-        200
-      );
-    },
-  )
   // add new track
   .openapi(
     {
       method: 'post',
-      path: '/add',
+      path: '/',
       description: 'Create a new track in the database',
       request: {
         body: {
@@ -116,77 +41,66 @@ export const trackRoute = new OpenAPIHono()
       );
     },  
   )
-  // delete track by id
+  // get all tracks
   .openapi(
     {
-      method: 'delete',
-      path: 'delete/{id}',
-      description: 'Delete track by id',
+      method: 'get',
+      path: '/',
+      description: 'Get all tracks',
+      responses: {
+        200: {
+          description: 'List of tracks',
+        },
+      },
+      tags: API_TAG, 
+    },
+    async (c) => {
+      const tracks = await trackService.getTracksWithAlbums();
+
+      return c.json(
+        {
+          status: 'success',
+          message: 'Success get all tracks',
+          data: tracks
+        },
+        200
+      );
+    },
+  )
+  .openapi(
+    {
+      method: 'get',
+      path: '/{id}',
+      description: 'Get track by id',
       request: {
         params: TrackIdSchema,
-      },
-       responses: {
-         200: {
-           description: 'Track deleted',
-         },
-         404: {
-          description: 'Track not found',
-         },
-       },
-       tags: API_TAG,
      },
-     async (c) => {
-       const trackId = Number(c.req.param('id'));
-
-       const exists = await trackService.isExists(trackId)
-
-       if (!exists) {
-          return c.json({ message: 'Artist not found' }, 404);
-       }
-
-       const deletedTrack = trackService.deleteTrackById(trackId);
-
-       return c.json(
-          {
-             status: "success",
-             message: "Successfully delete track",
-             data: deletedTrack,
-          }, 200);
-     },   
-  )
-  // delete all track
-  .openapi(
-      {
-        method: 'delete',
-        path: 'delete-all',
-        description: 'Delete all tracks',         
-        responses: {
-          200: {
-            description: 'All track deleted',
-          },
-          404: {
-            description: 'Track failure to delete',
-          },
+      responses: {
+        200: {
+          description: 'Details of track',
         },
-        tags: API_TAG,
       },
-      async (c) => {
+      tags: API_TAG, 
+    },
+    async (c) => {
+      const trackId = Number(c.req.param('id'));
+      const track = await trackService.getTrackById(trackId);
 
-        const deletedTracks = await trackService.deleteAllTracks();
-
-        return c.json(
-            {
-              status: "success",
-              message: "Successfully delete all tracks",
-              data: deletedTracks,
-            }, 200);
-      },   
+      return c.json(
+        {
+          status: 'success',
+          message: 'Success get track',
+          data: track
+        },
+        200
+      );
+    },
   )
   // update track
   .openapi(
     {
-      method: 'put',
-      path: 'update/{id}',
+      method: 'patch',
+      path: '/{id}',
       description: 'Update track by id',
       request: {
           params: TrackIdSchema,
@@ -227,4 +141,42 @@ export const trackRoute = new OpenAPIHono()
              data: updatedTrack,
           }, 200);
     },   
+  )
+  // delete track by id
+  .openapi(
+    {
+      method: 'delete',
+      path: '/{id}',
+      description: 'Delete track by id',
+      request: {
+        params: TrackIdSchema,
+      },
+       responses: {
+         200: {
+           description: 'Track deleted',
+         },
+         404: {
+          description: 'Track not found',
+         },
+       },
+       tags: API_TAG,
+     },
+     async (c) => {
+       const trackId = Number(c.req.param('id'));
+
+       const exists = await trackService.isExists(trackId)
+
+       if (!exists) {
+          return c.json({ message: 'Artist not found' }, 404);
+       }
+
+       const deletedTrack = trackService.deleteTrackById(trackId);
+
+       return c.json(
+          {
+             status: "success",
+             message: "Successfully delete track",
+             data: deletedTrack,
+          }, 200);
+     },   
   )
